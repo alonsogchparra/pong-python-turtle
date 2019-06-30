a683ccb70e9a51f4368aabef5b7ff9154c48a86e
Alonso Parra <alonsogparra@gmail.com>
2019-06-30 10:08:52 -0400

    Adding function btn_start_pressed to make the game start

--- pong.py
+++ pong.py
@@ -14,6 +14,7 @@ wn.tracer(0)
 paddle_a = turtle.Turtle()
 paddle_b = turtle.Turtle()
 pen = turtle.Turtle()
+set_goal= turtle.Turtle()
 ball = turtle.Turtle()
 ball.dx = 0
 ball.dy = 0
@@ -49,6 +50,68 @@ press_q.penup()
 press_q.hideturtle()
 press_q.goto(0, -100)
 press_q.write('Exit (Press Q)', align='center', font=('Courier', '36', 'bold'))
+
+# Functions
+
+def btn_start_pressed():
+
+  start_game.clear()
+  press_enter.clear()
+  press_q.clear()
+  player_a_wins.clear()
+  player_b_wins.clear()
+
+  ball.dx = 2
+  ball.dy = -2
+
+  # Paddle A
+
+  paddle_a.speed(0)
+  paddle_a.shape('square')
+  paddle_a.color('white')
+  paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+  paddle_a.penup()
+  paddle_a.goto(-350, 0)
+
+  # Paddle B
+
+  paddle_b.speed(0)
+  paddle_b.shape('square')
+  paddle_b.color('white')
+  paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+  paddle_b.penup()
+  paddle_b.goto(350, 0)
+
+  # Ball
+
+  ball.speed(0)
+  ball.shape('square')
+  ball.color('white')
+  ball.penup()
+  ball.goto(0, 0)
+
+  # Pen
+  pen.speed(0)
+  pen.color('white')
+  pen.penup()
+  pen.hideturtle()
+  pen.goto(0, 260)
+  pen.write('Player A: 0  Player B: 0', align='center', font=('Courier', 24, 'normal'))
+
+  # Set Goal
+  set_goal.speed(0)
+  set_goal.color('white')
+  set_goal.penup()
+  set_goal.hideturtle()
+  set_goal.goto(0, 250)
+  set_goal.write('Reach 5 points to WIN!', align='center', font=('Courier', 10, 'normal'))
+
+
+
+# Keyboard Binding
+
+wn.listen()
+wn.onkeypress(btn_start_pressed, 'Return')
 
 # Main Game Loop
 
